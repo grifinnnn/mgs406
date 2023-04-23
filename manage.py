e92fdb95462137036524857da45871cd76fccb07
Griffin <104595294+grifinnnn@users.noreply.github.com>
2023-04-23 20:05:55 +0000

    Week11 Assignment

--- manage.py
+++ manage.py
@@ -1,22 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/')
+# Route for the home page
+@app.route("/")
 def home():
-    return 'Welcome to my Flask app!'
+    return render_template("home.html")
 
-@app.route('/greetings/')
-def greetings():
-    return 'Please specify which greeting you would like to see by entering /greetings/christmas or /greetings/newyear.'
+# Route for the employee registration page
+@app.route("/registration")
+def registration():
+    return render_template("registration.html")
 
-@app.route('/greetings/christmas')
-def christmas():
-    return 'Merry Christmas!'
+# Route for the employee information page
+@app.route("/information")
+def information():
+    # Connect to the database
+    conn = sqlite3.connect("employees.db")
+    c = conn.cursor()
+    
+    # Query the database for all employees
+    c.execute("SELECT * FROM employee")
+    employees = c.fetchall()
+    
+    # Close the database connection
+    conn.close()
+    
+    # Render the information page with the employees table
+    return render_template("information.html", employees=employees)
 
-@app.route('/greetings/newyear')
-def newyear():
-    return 'Happy New Year!'
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
